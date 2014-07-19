9cd3be7a29e5e820b61a1b219356e7e750b46f61
data4sci <data4sci@gmail.com>
2014-07-19 12:04:58 +0200

    makeCacheMatrix(), cacheSolve() UPDATED; need to be commented!!!

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,25 +1,11 @@
-cacheSolve <- function() {
-  if (exists("inverseM") & exists("lastM") & exists("originalM")) { 
-                                        # having cached 3 matrix
-      if (identical(originalM, lastM)) {# & cached matrix didn't changed
-        message("getting cached data")
-        return(inverseM)                # returning cached inverse
-      }
-      else {                            # cached matrix have changed
-        message("cached matrix have changed - computing...")
-        inverseM <<- solve(originalM)   # computing & caching inverse 
-        lastM <<- originalM             # caching original matrix for later comparison 
-        return(inverseM)
-      }
+cacheSolve <- function(m, ...) {
+  invM <- m$getinverse()
+  if(!is.null(invM)) {
+    message("getting cached data")
+    return(invM)
   }
-  else if (exists("originalM")) {       # first use of function - nothing cached yet
-    message("first use - computing...")
-    inverseM <<- solve(originalM)       # computing & caching inverse   
-    lastM <<- originalM                 # caching original matrix for later comparison 
-    return(inverseM)
-  }
-  else {                                # matrix was not cached by makeCacheMatrix()
-    message("no cached matrix - run makeCacheMatrix() first.")
-    return(NA)
-  }
+  orgM <- m$get()
+  invM <- solve(orgM, ...)
+  m$setinverse(invM)
+  invM
 }
--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,3 +1,13 @@
 makeCacheMatrix <- function(m) {
-  originalM <<- m                       # saving argument (matrix)
-}                                       # into superior enviroment (cache)
+  invM <- NULL
+  set <- function(y) {
+    m <<- y
+    invM <<- NULL
+  }
+  get <- function() m
+  setinverse <- function(solve) invM <<- solve 
+  getinverse <- function() invM
+  list(set=set, get=get, 
+       setinverse=setinverse, 
+       getinverse=getinverse)
+}  
