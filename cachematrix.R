759aaf3d1c2ba5b9bec0688a02ad2bf3a2adffad
data4sci <data4sci@gmail.com>
2014-07-21 11:11:31 +0200

    cleaning up...

--- cachematrix.R
+++ cachematrix.R
@@ -2,32 +2,38 @@
 ## ProgrammingAssignment 2 @ class.coursera.org/rprog-005
 
 
-## This function is cashing argument (matrix) 
-## and its inverse in higher eviroment 
+##  function creates a special "matrix" object that 
+##  can cache its inverse
 
-makeCacheMatrix <- function(x = matrix) {
-  originalM <<- x                   # cashing argument (matrix)
-  inverseM  <<- solve(x)            # computing & caching 
-#  return(inverseM)
-}
+makeCacheMatrix <- function(m) {  # creatin "special matrix" 
+  invM <- NULL
+  set <- function(y) {            # $set
+    m <<- y                       # 
+    invM <<- NULL                 # 
+  }
+  get <- function() m             # $get
+  setinverse <- function(solve) invM <<- solve # $setinverse
+  getinverse <- function() invM   # $getinverse
+  list(set=set, get=get,          # return "special matrix"
+       setinverse=setinverse, 
+       getinverse=getinverse)
+}  
 
-## This function computes the inverse of the special "matrix"
-## returned by makeCacheMatrix above. If the inverse has already
+
+## function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix. If the inverse has already 
 ## been calculated (and the matrix has not changed), then 
 ## the cachesolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-  if (exists("originalM") & exists("inverseM")) { 
-                                            # was makeCacheMatrix
-                                            # used to compute and 
-                                            # cache inverse matrix?
 
-    if (identical(x, originalM)) {          # matrix changed? 
-      message("getting cached data")
-      return(inverseM)                      # use cached data
-    }
+cacheSolve <- function(m, ...) {  
+  invM <- m$getinverse()          # assign computed inverse 
+  if(!is.null(invM)) {            # if already calculated 
+    message("getting cached data")  
+    return(invM)                  # retrive cached inverse 
   }
-  else {                                    # matrix changed or 
-    return(solve(x))                        # wasn't cached 
-  }                                         # compute inverse
+  orgM <- m$get()                 # if wasn't calculated
+  invM <- solve(orgM, ...)        # calculate inverse form $get
+  m$setinverse(invM)              # $setinverse
+  invM                            # and retrive inverse  
 }
